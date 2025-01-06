bc8b0035e70afadfc214d9b5b94f2a306661f417
useless444 <habibhk127@gmail.com>
2025-01-06 23:49:37 +0530

    working with wish list

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import useProductDetails from "../../Hooks/CommonHooks/useProductDetails";
+import { Rating } from "@smastrom/react-rating";
+import { IoPeople } from "react-icons/io5";
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
 
 const ProductDetails = () => {
   const itemId = useParams();
@@ -12,10 +15,40 @@ const ProductDetails = () => {
       <div className="flex justify-center items-start gap-10 pt-10">
         <div className="flex flex-col justify-center items-center gap-5">
           <h1 className="text-2xl font-bold w-[80%]">{details?.name}</h1>
-          <img className="h-[300px] rounded-lg shadow-lg" src={details?.picture} alt="" />
+          <img
+            className="h-[300px] rounded-lg shadow-lg"
+            src={details?.picture}
+            alt=""
+          />
         </div>
         <div>
-          <div className="flex justify-start items-start gap-2">
+          <div className="flex flex-col justify-start items-start gap-4">
+            <div className="flex justify-start items-center gap-2">
+              <h1 className="text-2xl font-bold">Price:</h1>
+              <h1 className="text-2xl font-bold line-through text-error">
+                ${details?.price.previous_price}
+              </h1>
+              <h1 className="text-2xl font-bold">
+                ${details?.price.present_price}
+              </h1>
+            </div>
+            <div className="flex items-center gap-5">
+              <Rating
+                style={{ maxWidth: 150 }}
+                value={details?.rating}
+                readOnly
+              />
+              <div className="flex items-center gap-2">
+                <IoPeople className="text-2xl" />
+                <p>
+                  <span className="font-bold">{details?.total_rated}</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-5">
+            <BsFillBookmarkHeartFill className="text-3xl text-red-600" />
+            </div>
             <h1 className="font-bold">Description</h1>
             <p>{details?.description}</p>
           </div>
